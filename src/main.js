614835b5b95b50ae418b2b965ab6f5d467c95de2
taohua <liuy1994g@gmail.com>
2019-04-16 14:19:15 +0800

    finish ssr

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { createRouter } from "./router";
-
-// 导出一个工厂函数，用于创建新的
-// 应用程序、router 和 store 实例
-export function createApp () {
+import Vue from "vue";
+import App from "./App.vue";
+import { createRouter } from "@/router";
+import { createStore } from "@/store";
+export function createApp() {
   const router = createRouter();
+  const store = createStore()  // +
   const app = new Vue({
     router,
-    // 根实例简单的渲染应用程序组件。
+    store,      // +
     render: h => h(App)
-  })
-  return { app,router }
+  });
+  return { app, router,store };
 }
--- src/router.js
+++ src/router.js
@@ -14,6 +14,11 @@ export function createRouter(){
         component: Home
       },
       {
+        path: '/home',
+        name: 'home',
+        component: Home
+      },
+      {
         path: '/about',
         name: 'about',
         component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
